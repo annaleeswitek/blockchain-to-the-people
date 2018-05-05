8315999c4afbd283f93d79d38000e4ff1789a80f
Tasnuva Noor <noortasnuva@gmail.com>
2018-05-05 17:02:44 -0400

    user-home displaying active and upcoming elections

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import { fetchActiveElections, fetchUpcomingElections } from '../store/user-home';
@@ -6,24 +6,56 @@ import { fetchActiveElections, fetchUpcomingElections } from '../store/user-home
 /**
  * COMPONENT
  */
-export const UserHome = (props) => {
+class UserHome extends Component {
+  componentDidMount() {
+    this.props.getActiveElections();
+    this.props.getUpcomingElections();
+  }
+  render() {
+    console.log('PROPS', this.props)
+    let active = this.props.activeElections.filter(election => election.communityId === this.props.user.communityId)
+
+    let upcoming = this.props.upcomingElections.filter(election => election.communityId === this.props.user.communityId)
 
   return (
     <div>
-      <h3>Welcome, {props.user.name}</h3>
+      <h3>Welcome, {this.props.user.name}</h3>
       <hr />
       <h4>Active Election:</h4>
-      <h5>election name</h5>
-      <h5>election date</h5>
-      <h5>candidate list</h5>
-      <button>Vote Now!</button>
+      {
+        active.length
+        ? active.map(election => {
+            return (
+              <div key={election.id}>
+                <h5>{election.name}</h5>
+                <h5>From: {election.startDate}</h5>
+                <h5>To: {election.endDate}</h5>
+                <button>Vote Now!</button>
+              </div>
+            )
+          })
+        : <div>"There's no active election in your community!"</div>
+      }
       <hr />
       <h4>Upcoming Elections:</h4>
-      <h5>election name</h5>
-      <h5>election date</h5>
-      <h5>candidate list</h5>
+      {
+        upcoming.length
+        ? upcoming.map(election => {
+            return (
+              <div key={election.id}>
+                <h5>{election.name}</h5>
+                <h5>From: {election.startDate}</h5>
+                <h5>To: {election.endDate}</h5>
+              </div>
+            )
+          })
+        : <div>There's no upcoming elections in your community!</div>
+      }
     </div>
   )
+
+  }
+  
 }
 
 /**
