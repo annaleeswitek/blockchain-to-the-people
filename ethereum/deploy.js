60f234895310ddf4de356da9feea2c89efc8f513
Melissa Bellah <melissabellah@gmail.com>
2018-05-07 11:33:57 -0400

    deploy commented out

---
+++ ethereum/deploy.js
@@ -0,0 +1,18 @@
+// const assert = require('assert');
+// const ganache = require('ganache-cli');
+// const Web3 = require('web3');
+
+// const compiledFactory = require('../ethereum/build/ElectionFactory.json');
+
+// const Provider =
+// let accounts;
+// let factory;
+// let electionAddress;
+// let election;
+
+// accounts = await web3.eth.getAccounts();
+
+// factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+//   .deploy({ data: compiledFactory.bytecode })
+//   .send({ from: accounts[0], gas: '1000000' });
+
