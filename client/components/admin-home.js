dd8f28730cff8c20f6b21f905ca9e88d333785a5
annaleeswitek <annaleejude@gmail.com>
2018-05-07 19:54:45 -0400

    changed fav icon

--- client/components/admin-home.js
+++ client/components/admin-home.js
@@ -1,7 +1,25 @@
 import React, { Component } from 'react'
 
 export default class AdminHome extends Component {
-  render() {
-    return(<div>Admin Home!</div>)
+  constructor(){
+    super();
+    this.state = {
+      name: '',
+      startDate: '',
+      endDate: ''
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <h1>New Election</h1>
+          <form>
+            <label>Name</label>
+            <input type= "text" />
+          </form>
+          <button>Submit</button>
+      </div>
+  )
   }
 }
