02b16ee6de1669fb94e112355b1e32ce97235570
annaleeswitek <annaleejude@gmail.com>
2018-05-15 12:53:47 -0400

    resolved merge conflict

--- ethereum/factory.js
+++ ethereum/factory.js
@@ -3,12 +3,7 @@ import ElectionFactory from './build/ElectionFactory.json';
 
 const contractInstance = new web3.eth.Contract(
   JSON.parse(ElectionFactory.interface),
-<<<<<<< HEAD
   '0xee0B5D81C34833D0e9E59BfB68b216A98E58D2ed'
-=======
-  '0x6Bef635dbb8966d7a92bBE6eA2B3b2cDD15740CA'
-
->>>>>>> 0e04675c18750a3d425f65a6c986e70f1d12aae2
 );
 
 export default contractInstance;
